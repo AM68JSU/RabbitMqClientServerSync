c448b73ecf26a271b867fa586b6182a45e0631e0
Akbar Mazrouei <Akbar_Mazrouei@hotmail.com>
2024-10-03 10:06:12 +0330

    DbContextFactory (ServerDbContext,ClientDbContext)

--- ClientServerSyncRabbitMq.Infrastructure/Data/ClientDbContext.cs
+++ ClientServerSyncRabbitMq.Infrastructure/Data/ClientDbContext.cs
@@ -1,10 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ClientServerSyncRabbitMq.Domain.Entities;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
 public class ClientDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
 
     public ClientDbContext(DbContextOptions<ClientDbContext> options) : base(options) { }
+
+
+    public class ClientDbContextFactory : IDesignTimeDbContextFactory<ClientDbContext>
+    {
+        public ClientDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ClientDbContext>();
+            optionsBuilder.UseSqlServer("Password=123asd/;Persist Security Info=True;User ID=sa;Initial Catalog=ClientDB;Data Source=192.168.10.57;TrustServerCertificate=true;");
+
+            return new ClientDbContext(optionsBuilder.Options);
+        }
+    }
 }
--- ClientServerSyncRabbitMq.Infrastructure/Data/ServerDbContext.cs
+++ ClientServerSyncRabbitMq.Infrastructure/Data/ServerDbContext.cs
@@ -1,10 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ClientServerSyncRabbitMq.Domain.Entities;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Design;
 
 public class ServerDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
 
     public ServerDbContext(DbContextOptions<ServerDbContext> options) : base(options) { }
+
+
+    public class ServerDbContextFactory : IDesignTimeDbContextFactory<ServerDbContext>
+    {
+        public ServerDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ServerDbContext>();
+            optionsBuilder.UseSqlServer("Password=123asd/;Persist Security Info=True;User ID=sa;Initial Catalog=ServerDB;Data Source=192.168.10.57;TrustServerCertificate=true;");
+
+            return new ServerDbContext(optionsBuilder.Options);
+        }
+    }
 }
